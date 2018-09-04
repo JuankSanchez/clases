c0c7b5962c53b62ea5415d981d0f144f33358d76
Juan Sanchez <juankcba@hotmail.com>
2018-09-04 16:01:16 -0300

    Anexo jquery

--- js/ejemplos.jquery.js
+++ js/ejemplos.jquery.js
@@ -18,6 +18,12 @@ window.addEventListener('load',function(){
 	h1.addEventListener('click',function(){
 		alert('click desde js');
 	});
+
+	// asignar el evento al boton js
+	var btnjs = document.getElementById('btnjs');
+
+	btnjs.addEventListener('click',showJS);
+
 });
 
 // forma jquery
@@ -27,7 +33,39 @@ $(document).ready(function(){
 	hq.css('color','orange');
 	hq.click(function(){
 		//alert('click desde jq');
-	hq.fadeOut("slow", function () {console.log("hola")});
+		hq.fadeOut("slow", function () {console.log("hola")});
 	})
 	
+	var btonjq = $('#btnjq');
+	btonjq.click(hiddenJQ);
 });
+
+function showJS(){
+	var ns = document.querySelectorAll('p');
+	for(let n in ns){
+		ns[n].style='display:block';	
+	}
+}
+
+function hiddenJQ(){
+/* 
+	SELECTORES JQUERY
+	----------------
+
+	$("p.intro")				Todos las etiquetas <p> con class="intro"	
+	$("p:first")				Primer elemento <p>	
+	$("ul li:first")			Primer elmento <li> de <ul>	
+	$("ul li:first-child")		Primer <li> de cada <ul>	
+	$("[href]")					Todos los elementos con atributo href	
+	$("a[target='_blank']")		Todos los elementos <a> con target = "_blank"	
+	$("a[target!='_blank']")	Todos los elementos <a> con target != "_blank"
+	$(":button")				Todos los elementos <button> e  <input> con type="button"	
+	$("tr:even")				Etiquetas impares <tr>	
+	$("tr:odd")					Etiquetas pares <tr>
+
+*/
+	$("p").hide();
+}
+
+
+
